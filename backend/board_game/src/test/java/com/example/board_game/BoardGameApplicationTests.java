0f8ebc6e68cdcd933f744c8cb76f77ea622b73c8
enrouteeee <enrouteeee@gmail.com>
2022-03-11 20:34:33 +0900

    first commit : backend

---
+++ backend/board_game/src/test/java/com/example/board_game/BoardGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.board_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
