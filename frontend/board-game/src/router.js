285fd1a4e478e623596a9b86ffc6505dbf59dbb8
enrouteeee <enrouteeee@gmail.com>
2022-03-28 17:58:29 +0900

    Feat : 기본 라이브러리 및 설정

---
+++ frontend/board-game/src/router.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/views/LoginView.vue'
+import RoomList from '@/views/RoomListView.vue'
+import Room from '@/views/RoomView.vue'
+import GameDavinci from '@/views/GameDavinci.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: Login
+    },
+    {
+      path: '/room-list',
+      component: RoomList
+    },
+    {
+      path: '/room',
+      component: Room
+    },
+    {
+      path: '/game/davinci',
+      component: GameDavinci
+    }
+  ]
+})
